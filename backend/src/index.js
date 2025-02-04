55f4d502bdc344280d56c8b60c5ef43d1c021c02
ReshmaZore <zorereshma095@gmail.com>
2025-02-04 17:20:35 +0530

    login,logout routes added

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,13 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js"
-import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT
+
+app.use(express.json())
  
 app.use("/api/auth", authRoutes);
 
