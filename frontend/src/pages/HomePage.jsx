2fbbc50a54dacfd81b36d5fc839d2227debe35d3
ReshmaZore <zorereshma095@gmail.com>
2025-02-05 08:56:41 +0530

    second commit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
 import { useChatStore } from "../store/useChatStore";
 
+import Sidebar from "../components/Sidebar";
+import NoChatSelected from "../components/NoChatSelected";
+import ChatContainer from "../components/ ChatContainer";
 
 const HomePage = () => {
   const { selectedUser } = useChatStore();
@@ -11,11 +13,12 @@ const HomePage = () => {
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
             <Sidebar />
-            </div>
-            </div>
-            </div>
-            </div>
-  )
-}
 
-export default HomePage
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default HomePage;
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -64,5 +64,5 @@ export const useChatStore = create((set, get) => ({
     socket.off("newMessage");
   },
 
-  setSelectedUser: (selectedUser) => set({ selectedUser }),
+ setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
